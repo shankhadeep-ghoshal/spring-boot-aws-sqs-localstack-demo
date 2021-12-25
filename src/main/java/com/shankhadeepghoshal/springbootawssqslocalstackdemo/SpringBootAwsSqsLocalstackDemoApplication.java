1f7face3cea28b560ac0d57195a9a7fd13af6a6e
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2021-12-25 23:49:36 +0300

    initial commit

---
+++ src/main/java/com/shankhadeepghoshal/springbootawssqslocalstackdemo/SpringBootAwsSqsLocalstackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shankhadeepghoshal.springbootawssqslocalstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsSqsLocalstackDemoApplication {
+
+		public static void main(String[] args) {
+				SpringApplication.run(SpringBootAwsSqsLocalstackDemoApplication.class, args);
+		}
+
+}
---
+++ src/test/java/com/shankhadeepghoshal/springbootawssqslocalstackdemo/SpringBootAwsSqsLocalstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankhadeepghoshal.springbootawssqslocalstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAwsSqsLocalstackDemoApplicationTests {
+
+		@Test
+		void contextLoads() {
+		}
+
+}
